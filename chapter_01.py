b4aefe3605ece7676158cf58cc54d3b81ac0ef0e
Gus <gustavo.kildegaard@gmail.com>
2023-06-28 16:32:19 -0300

    Vistos temas de bordes, dilatación, erosión y alguna que otra cosa más

--- chapter_01.py
+++ chapter_01.py
@@ -1,5 +1,6 @@
 import cv2
 import time
+import numpy as np
 print('Package imported')
 
 # Leer imágenes
@@ -27,18 +28,58 @@ print('Package imported')
 
 # Uso de la webcam
 
-# 0 es para la webcam, otros números es por si tenés más cámaras
-cap = cv2.VideoCapture(0)
+# # 0 es para la webcam, otros números es por si tenés más cámaras
+# cap = cv2.VideoCapture(0)
+#
+# # Los settings tienen códigos. 3 es para width y 4 es para height
+# cap.set(3, 640)
+# cap.set(4, 480)
+#
+# # También hay para el brillo:
+# cap.set(10, 50)
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Webcam", img)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-# Los settings tienen códigos. 3 es para width y 4 es para height
-cap.set(3, 640)
-cap.set(4, 480)
+# Convertir a grises una imagen
 
-# También hay para el brillo:
-cap.set(10, 50)
-while True:
-    success, img = cap.read()
-    cv2.imshow("Webcam", img)
+img = cv2.imread('./resources/lena.png')
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# función para convertir colores
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# cv2.imshow('Imagen en grises', img_gray)
+# cv2.waitKey(1000)
+
+# Le podemos agregar gaussian blur!
+
+img_blur = cv2.GaussianBlur(img_gray, ksize=(7, 7), sigmaX=0)
+# cv2.imshow('Imagen con blureo con 7', img_blur)
+
+img_blur = cv2.GaussianBlur(img_gray, ksize=(15, 15), sigmaX=0)
+# cv2.imshow('Imagen con blureo con 15', img_blur)
+
+# cv2.waitKey(0)
+
+# Ahora vamos a jugar con EDGE DETECTOR
+
+img_canny = cv2.Canny(img, 150, 150)
+cv2.imshow('Detectando bordes', img_canny)
+
+# cv2.waitKey(0)
+
+# Los bordes que detecta los podemos modificar con los argumentos.
+# Estos bordes los podemos engrosar ("DILATE") o afinar ("ERODE")
+
+kernel = np.ones(shape=(5, 5), dtype=np.uint8)
+
+img_dilatada = cv2.dilate(img_canny, kernel=kernel, iterations=1)
+img_eroded = cv2.erode(img_dilatada, kernel=kernel, iterations=1)
+
+cv2.imshow('Imagen dilatada', img_dilatada)
+cv2.imshow('Imagen erosionada', img_eroded)
+
+cv2.waitKey(0)
